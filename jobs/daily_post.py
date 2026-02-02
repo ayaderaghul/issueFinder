30ab30c7ef969080eec5831a0a26d993e01c5ef5
Aya de Raghũl <nguyenlinhchi09@gmail.com>
2026-02-02 09:34:51 +0100

    reduce number of issues

--- jobs/daily_post.py
+++ jobs/daily_post.py
@@ -14,7 +14,7 @@ def search_issues(query):
         "q": query,
         "sort": "created",
         "order": "desc",
-        "per_page": 10,
+        "per_page": 8,
     }
     res = requests.get(GITHUB_SEARCH_URL, params=params)
     res.raise_for_status()
